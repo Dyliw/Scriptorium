2222c4a965a0c5f0994585ea3e462c290170419e
Deysy <deysygmtz.dm@gmail.com>
2026-07-06 22:16:03 -0600

    Create routes.py

---
+++ backend/app/api/routes.py
@@ -0,0 +1,13 @@
+from fastapi import APIRouter
+import logging
+from app.api.v1 import auth, book, user, progress, session, word
+
+
+api_router = APIRouter()
+
+api_router.include_router(auth.router, prefix="/auth", tags=["Autenticación"])
+api_router.include_router(book.router, prefix="/books", tags=["Libros"])
+api_router.include_router(user.router, prefix="/user", tags=["Usuarios"])
+api_router.include_router(progress.router, prefix="/progress", tags=["Progreso"])
+api_router.include_router(session.router, prefix="/session", tags=["Sesión"])
+api_router.include_router(word.router, prefix="/word", tags=["Palabras"])
