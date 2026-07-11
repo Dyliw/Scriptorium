b9f64032e9c1759e91e8404b4c7cfe0a1e4f41b3
Deysy <deysygmtz.dm@gmail.com>
2026-07-10 22:35:04 -0600

    Update useTimer.js

--- frontend/src/hooks/useTimer.js
+++ frontend/src/hooks/useTimer.js
@@ -38,7 +38,41 @@ export const useTimer = (initalDuration = 0)=>{
     const reset = useCallback((resetValue=0)=>{
         if(intervalRef.current){
             clearInterval(intervalRef.current);
-            intervalRef.current = null;sa
+            intervalRef.current = null;
         }
-    })
-}
+
+        setIsRunning(false);
+        setTime(resetValue);
+        timeRef.current = resetValue;
+    }, []);
+
+    const setDuration = useCallback((duration)=>{
+        if(typeof duration !== 'number' || duratoin < 0){
+            console.warn('La duración debe de ser un número positivo');
+            return;
+        }
+        if (intervalRef.current){
+            clearInterval(intervalRef.current);
+            intervalRef.current = null;
+        }
+        setIsRunning(false);
+        setTime(duration);
+        timeRef.current = duration;
+    }, []);
+
+    const formatTime = useCallback(()=>{
+        const hours = Math.floor(time/3600);
+        const minutes = Math.floor((time % 3600) / 60);
+        const seconds = time %60;
+        const formattedHours = hours.toString().padStart(2, '0');
+        const formattedMinutes = minutes.toString().padStart(2, '0');
+        const formattedSeconds = seconds.toString().padStart(2, '0');
+
+        return `${formattedHours}: ${formattedMinutes}: ${formattedSeconds}`;
+
+    }, [time]);
+    return{
+        time,
+        isRunning, start, pause, reset, setDuration, formatTime
+    };
+};
